358f8e1f40678e667a1685344299ddf2281f3f02
Clecio Silva <clecio-sf@hotmail.com>
2025-03-20 11:48:49 -0300

    feat(history): adiciona caso de uso para histórico com paginação e testes unitários

---
+++ src/use-cases/fetch-user-check-ins-history.ts
@@ -0,0 +1,26 @@
+import { CheckIn } from '@prisma/client'
+import { CheckInsRepository } from '@/repositories/check-ins-repository'
+
+interface FetchUserCheckInsHistoryUseCaseRequest {
+  userId: string
+  page: number
+}
+
+interface FetchUserCheckInsHistoryUseCaseResponse {
+  checkIn: CheckIn[]
+}
+
+export class FetchUserCheckInsHistoryUseCase {
+  constructor(private chekInsRepository: CheckInsRepository) {}
+
+  async execute({
+    userId,
+    page,
+  }: FetchUserCheckInsHistoryUseCaseRequest): Promise<FetchUserCheckInsHistoryUseCaseResponse> {
+    const checkIn = await this.chekInsRepository.findManyByUser(userId, page)
+
+    return {
+      checkIn,
+    }
+  }
+}
