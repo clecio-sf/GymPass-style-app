6807a845abe1a1ef8ee9db6df7034b9af78bc1b7
Clecio Silva <clecio-sf@hotmail.com>
2025-03-25 18:15:05 -0300

    test(check-in): add e2e tests for creation, metrics, validation, and history

---
+++ src/http/controllers/check-ins/create.spec.ts
@@ -0,0 +1,37 @@
+import request from 'supertest'
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
+import { app } from '@/app'
+import { createAndAuthenticateUser } from '@/utils/test/create-and-authenticate-user'
+import { prisma } from '@/lib/prisma'
+
+describe('Create Check-in (e2e)', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('should be able to create a check-in', async () => {
+    const { token } = await createAndAuthenticateUser(app)
+
+    const gym = await prisma.gym.create({
+      data: {
+        title: 'Js Gym',
+        latitude: -14.8478133,
+        longitude: -40.867746,
+      },
+    })
+
+    const response = await request(app.server)
+      .post(`/gyms/${gym.id}/check-ins`)
+      .set('Authorization', `Bearer ${token}`)
+      .send({
+        latitude: -14.8478133,
+        longitude: -40.867746,
+      })
+
+    expect(response.statusCode).toEqual(201)
+  })
+})
