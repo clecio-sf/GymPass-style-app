9bc4150a21eb6b99e0f801be527a1a8178717664
Clecio Silva <clecio-sf@hotmail.com>
2025-03-25 10:52:10 -0300

    feat(gym): add controllers for search and nearby gyms

--- src/http/controllers/gyms/routes.ts
+++ src/http/controllers/gyms/routes.ts
@@ -1,6 +1,13 @@
 import { FastifyInstance } from 'fastify'
 import { jwtVerify } from '@/http/middlewares/verify-jwt'
+import { search } from './search'
+import { nearby } from './nearby'
+import { create } from './create'
 
 export async function gymsRoutes(app: FastifyInstance) {
   app.addHook('onRequest', jwtVerify)
+
+  app.get('/gyms/search', search)
+  app.get('/gyms/nearby', nearby)
+  app.post('/gyms', create)
 }
