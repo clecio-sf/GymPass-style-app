a1874d977f8a7dae661142bd71eb906a32829561
Clecio Silva <clecio-sf@hotmail.com>
2025-03-25 17:21:40 -0300

    test(gym): add e2e tests for gym creation, search, and nearby search

---
+++ src/http/controllers/gyms/create.spec.ts
@@ -0,0 +1,56 @@
+import request from 'supertest'
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
+import { app } from '@/app'
+import { createAndAuthenticateUser } from '@/utils/test/create-and-authenticate-user'
+
+describe('Search gyms (e2e)', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('should be able to create a gym', async () => {
+    const { token } = await createAndAuthenticateUser(app)
+
+    await request(app.server)
+      .post('/gyms')
+      .set('Authorization', `Bearer ${token}`)
+      .send({
+        title: 'Js gym',
+        description: 'something',
+        phone: '1234567890',
+        latitude: -14.8478133,
+        longitude: -40.867746,
+      })
+
+    await request(app.server)
+      .post('/gyms')
+      .set('Authorization', `Bearer ${token}`)
+      .send({
+        title: 'Ts gym',
+        description: 'something',
+        phone: '1234567890',
+        latitude: -14.8478133,
+        longitude: -40.867746,
+      })
+
+    const response = await request(app.server)
+      .get('/gyms/search')
+      .query({
+        q: 'Js',
+      })
+      .set('Authorization', `Bearer ${token}`)
+      .send()
+
+    expect(response.statusCode).toEqual(200)
+    expect(response.body.gyms).toHaveLength(1)
+    expect(response.body.gyms).toEqual([
+      expect.objectContaining({
+        title: 'Js gym',
+      }),
+    ])
+  })
+})
--- src/http/controllers/gyms/search.spec.ts
+++ src/http/controllers/gyms/search.spec.ts
@@ -3,7 +3,7 @@ import { afterAll, beforeAll, describe, expect, it } from 'vitest'
 import { app } from '@/app'
 import { createAndAuthenticateUser } from '@/utils/test/create-and-authenticate-user'
 
-describe('Profile (e2e)', () => {
+describe('Search gym (e2e)', () => {
   beforeAll(async () => {
     await app.ready()
   })
@@ -12,19 +12,20 @@ describe('Profile (e2e)', () => {
     await app.close()
   })
 
-  it('should be able to get user profile', async () => {
+  it('should be able to search gyms by title', async () => {
     const { token } = await createAndAuthenticateUser(app)
 
-    const profileResponse = await request(app.server)
-      .get('/me')
+    const response = await request(app.server)
+      .post('/gyms')
       .set('Authorization', `Bearer ${token}`)
-      .send()
+      .send({
+        title: 'JS gym',
+        description: 'something',
+        phone: '1234567890',
+        latitude: -14.8478133,
+        longitude: -40.867746,
+      })
 
-    expect(profileResponse.statusCode).toEqual(200)
-    expect(profileResponse.body.user).toEqual(
-      expect.objectContaining({
-        email: 'jao@gmail.com',
-      }),
-    )
+    expect(response.statusCode).toEqual(200)
   })
 })
